194205e90db63f9be67ddd366b4b33cb3833322e
Jianxff <3109441270@qq.com>
2023-05-01 23:55:32 +0800

    [U] update highlight

--- src/components/C-Highlight.vue
+++ src/components/C-Highlight.vue
@@ -1,19 +1,13 @@
 <template>
   <highlightjs 
-    language="c"
+    autodetect
   />
 </template>
 
-<script>
+<script setup>
 // import 'highlight.js/styles/stackoverflow-light.css'
 import 'highlight.js/styles/github-dark-dimmed.css'
 import 'highlight.js/lib/common';
 import hljsVuePlugin from "@highlightjs/vue-plugin";
-export default {
-  data() {
-  },
-  components: {
-    highlightjs: hljsVuePlugin.component
-  }
-}
+const highlightjs = hljsVuePlugin.component;
 </script>
